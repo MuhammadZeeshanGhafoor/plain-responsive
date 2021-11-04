039cc376822d0ce9b09b887464383714b0e72bbf
MuhammadZeeshanGhafoor <mirzazeeshan3738533@gmail.com>
2021-11-05 04:15:28 +0500

    update2

--- src/Pages/components/navbar/index.js
+++ src/Pages/components/navbar/index.js
@@ -1,7 +1,38 @@
+import React, {useState} from "react";
 import logo from "../../../assets/logo.svg";
 import barslogo from "../../../assets/bars-solid.svg";
+import xmark from "../../../assets/xmark-solid.svg";
 
 const NavigationBar = () => {
+
+  const [showMenu, setShowMenu] = useState(false);
+
+let menu 
+
+if (showMenu){
+  menu = <div className="w-full h-80 -mt-40 bg-red-500 flex flex-col justify-center fixed">
+    <div className="w-full flex h-10 justify-end "><img src={xmark} className="w-10 bg-white mr-4 rounded-full " onClick={()=>{setShowMenu(false)}}></img></div>
+    <ul className="text-white text-lg justify-center w-full flex flex-col">
+            <li className="text-center hover:text-gray-800 " >Home</li>
+            
+            <li className="text-center hover:text-gray-800 tracking-widest">About </li>
+            
+            <li className="text-center hover:text-gray-800 tracking-widest">Privacy</li>
+            
+            <li className="text-center hover:text-gray-800 tracking-widest">Service</li>
+            
+            <li className="text-center hover:text-gray-800 tracking-widest">Price</li>
+            
+            <li className="text-center hover:text-gray-800 tracking-widest">Contact</li>
+            <button className=" bg-gray-500 rounded-full h-12 text-black font-semibold">Register</button>
+            <button className="underline">login</button>
+    </ul>
+  </div>
+}
+else {
+  menu = <div className="hidden">Menu is this</div>
+}
+
   return (
     <div>
         
@@ -18,8 +49,9 @@ const NavigationBar = () => {
           <button className="bg-red-500 w-32 h-14 text-white">Register</button>
           <button className="bg-red-500 w-32 h-14 text-white">login</button>
           </div>
-        <img src={barslogo} className="w-5 lg:hidden"></img>
+        <img src={barslogo} className="w-5 lg:hidden" onClick={()=>setShowMenu(!showMenu)}></img>
       </div>
+        {menu}
     </div>
   );
 };
