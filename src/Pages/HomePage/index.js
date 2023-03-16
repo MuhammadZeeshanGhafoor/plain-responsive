dc4eda0278ad467e2105d16907a1ffb5d9b7c688
Muhammad Zeeshan Ghafoor <81298391+MuhammadZeeshanGhafoor@users.noreply.github.com>
2023-03-17 00:02:03 +0500

    Updated Phone number

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -33,7 +33,7 @@ const HomePage = () => {
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna.
           </p>
-          <a href="whatsapp://send?abid=phonenumber&text=Hello%2C%20World!" className="w-40 h-14 bg-gray-700 text-white rounded-full rounded-tr-none mt-10 ">
+          <a href="whatsapp://send?abid=+923084075324&text=Hello%2C%20World!" className="w-40 h-14 bg-gray-700 text-white rounded-full rounded-tr-none mt-10 ">
             Get Started!
           </a>
         </div>
